658080d3a5a6cdb59a4ec76f281193f2339351ce
Saif-Ur-Rehmaan <FSDev.SaifUrRehman@gmail.com>
2025-05-28 03:21:03 +0500

    Q3 solved

---
+++ question_3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**Q. 3. Write a program that reads three user inputted numbers and prints the largest number. */
+
+int main()
+{
+    printf("\nQ.3. Write a program that reads three user inputted numbers and prints the largest number.  \n");
+    printf("\n* Enter 0 to Exit and  \n");
+    printf("\n* Any other numbers to continue game..  \n");
+    int lastMax=0;
+  while(1){
+      int input;
+      if(input==0){
+          break;
+      }
+    printf("Enter: ");
+    scanf("%d",&input);
+
+    lastMax=lastMax<input?input:lastMax;
+    printf("\n");
+      
+  }
+    printf("Max is %d",lastMax);
+    return 0;
+}
+
+
